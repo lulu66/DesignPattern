93b94b878035d4240d9dc2c66cb41655c20291c2
lulu66 <1090087819@qq.com>
2022-06-27 00:07:17 +0800

    状态模式

---
+++ ConsoleApp1/ConsoleApp1/状态模式/StateMachine.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+	//1.找出所有状态
+	//2.创建实例变量持有目前的状态,定义每个状态的值
+
+	//简单糖果售卖机
+	public class SimpleStateMachine
+	{
+		static int SOLD_OUT = 0;
+		static int NO_QUARTER = 1;
+		static int HAS_QUARTER = 2;
+		static int SOLD = 3;
+
+		int state = SOLD_OUT;
+		int count = 0;
+
+		public SimpleStateMachine(int count)
+		{
+			this.count = count;
+			if(count > 0)
+			{
+				state = NO_QUARTER;
+			}
+		}
+	}
+}
